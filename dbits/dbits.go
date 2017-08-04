0dd40321be7fba524b0d620061a1923090cda514
Titus Tienaah <titus.intdxdt@gmail.com>
2017-08-03 23:33:43 -0300

    pack to float

--- dbits/dbits.go
+++ dbits/dbits.go
@@ -1,4 +1,4 @@
-package db
+package dbits
 
 import (
 	"bytes"
@@ -22,4 +22,19 @@ func DoubleBits(n float64) [2]uint32 {
 	}
 
 	return [2]uint32{a, b}
+}
+
+func Pack(lo uint32, hi uint32) float64 {
+	var buf bytes.Buffer
+	var f float64
+	if err := binary.Write(&buf, binary.LittleEndian, lo); err != nil {
+		panic(err)
+	}
+	if err := binary.Write(&buf, binary.LittleEndian, hi); err != nil {
+		panic(err)
+	}
+	if err := binary.Read(&buf, binary.LittleEndian, &f); err != nil {
+		panic(err)
+	}
+	return f
 }
