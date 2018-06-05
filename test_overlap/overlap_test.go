38456944e01f85604be3ef95013c206a6f35c31b
Titus Tienaah <titus.intdxdt@gmail.com>
2018-06-05 00:34:32 -0300

    test overlap

--- test_overlap/overlap_test.go
+++ test_overlap/overlap_test.go
@@ -9,7 +9,7 @@ import (
 func TestFloatOverlap(t *testing.T) {
 	g := goblin.Goblin(t)
 	g.Describe("Test Float Overlap", func() {
-		g.It("test double bits", func() {
+		g.It("test float overlap", func() {
 			g.Assert(TestOverlap(1.5, 0.5))
 			g.Assert(TestOverlap(math.Pow(2, -52), 1.0+math.Pow(2, -52)))
 			g.Assert(!TestOverlap(1.0, 0.5))
@@ -19,12 +19,10 @@ func TestFloatOverlap(t *testing.T) {
 			g.Assert(!TestOverlap(0.0, 0.0))
 
 			//test denormalized numbers
-			//underflow - in rust  float64MIN_EXP == -1021
-			//
-			//g.Assert(!TestOverlap(2float64.powi(-1024), 2float64.powi(-1023)));
-			//g.Assert(!TestOverlap(2float64.powi(-1023), 2float64.powi(-1022)));
-			//g.Assert(TestOverlap( 2float64.powi(-1024) + 2float64.powi(-1040), 2float64.powi(-1030)));
-			//g.Assert(!TestOverlap(2float64.powi(-1030) - 2float64.powi(-1031), 2float64.powi(-1030)));
+			g.Assert(!TestOverlap(math.Pow(2, -1024), math.Pow(2,-1023)))
+			g.Assert(!TestOverlap(math.Pow(2,-1023),  math.Pow(2,-1022)))
+			g.Assert(TestOverlap( math.Pow(2,-1024) + math.Pow(2,-1040),  math.Pow(2,-1030)))
+			g.Assert(!TestOverlap(math.Pow(2,-1030) - math.Pow(2,-1031),  math.Pow(2,-1030)))
 
 		})
 	})
